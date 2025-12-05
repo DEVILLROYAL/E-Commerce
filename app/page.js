d9558f1ef8098ffa3cd95ea16a02803ac879b174
devillroyal <deepakyadav4014@gmail.com>
2025-12-05 07:33:43 +0530

    fixd seo

--- app/page.js
+++ app/page.js
@@ -1,21 +1,11 @@
-import Main from "./layouts/components/Main";
+import FoodRadioSlider from "./layouts/components/FoodRadioSlider";
+import { Hero } from "./layouts/components/Hero";
+
 export default async function Home() {
-
-try{
-    const ul = "https://fakestoreapi.com/products";
-    const data = await fetch(ul);
-    const res = await data.json();
-    return (
-      <>
-      <Main data={res} />
-      </>
-    );
-} catch(error) {
-  console.log(error);
-}
-
   return (
     <>
+    <Hero/>
+    <FoodRadioSlider/>
     </>
   );
 }
